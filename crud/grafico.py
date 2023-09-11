e836de411c3c20ad01ea84e51ee87810df663a72
Mikenson <thomasmikenson40@gmail.com>
2023-09-10 23:11:02 -0300

    c

--- crud/grafico.py
+++ crud/grafico.py
@@ -1,7 +1,3 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
 from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
@@ -13,8 +9,6 @@ colors = {
     'text': '#7FDBFF'
 }
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
